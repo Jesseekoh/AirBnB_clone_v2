4c779971bbe379df82ea60365958aabf876ac721
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-12-08 00:39:38 +0100

    task 0: fix 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,8 @@
 sudo apt update -y
 sudo apt -y upgrade
 sudo apt -y install nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "Hello, this is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
